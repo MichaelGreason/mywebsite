e4c73590e3f6a50a12306bef74106a47aef9d859
Michael Greason <mgreason18@gmail.com>
2023-06-28 16:55:51 -0400

    about to create database with django and postgres

--- my-website/src/pages/home.jsx
+++ my-website/src/pages/home.jsx
@@ -5,7 +5,19 @@ import TextField from "@mui/material/TextField";
 export default function Home() {
   return (
     <>
-      <div>Hello World!</div>
+      <div>
+        <div className="flex justify-center items-center h-screen">
+          <Box>
+            <TextField
+              id="outlined-multiline-flexible"
+              multiline
+              maxRows={100}
+              className=""
+              size=""
+            ></TextField>
+          </Box>
+        </div>
+      </div>
     </>
   );
 }

b8784d14bc41db60a9ff9feaec7eb06497db50b9
Michael Greason <mgreason18@gmail.com>
2023-06-29 11:34:51 -0400

    installed moment

--- my-website/src/pages/blog.jsx
+++ my-website/src/pages/blog.jsx
@@ -1,9 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 import axios from "axios";
 
 export default function Blog() {
+  const [posts, setPosts] = useState([]);
+
   useEffect(() => {
     axios
       .get("http://127.0.0.1:8000/blog-post", {
@@ -13,11 +18,12 @@ export default function Blog() {
       })
       .then((response) => {
         console.log(response);
+        setPosts(response.data);
       })
       .catch((error) => {
         console.error(error);
       });
-  });
+  }, []);
 
   // function handlePost() {
   //   axios.post("http://127.0.0.1:8000/blog-post", {
@@ -29,19 +35,30 @@ export default function Blog() {
   //     console.log(response)
   //   }));
   // }
-  return (
-    <>
-      <div>
-        <div className="flex justify-center items-center h-screen">
-          <TextField
-            id="fullWidth"
-            multiline
-            rows={8}
-            maxRows={100}
-            className=" w-2/3"
-          ></TextField>
+
+  if (posts) {
+    return (
+      <>
+        <div>
+          <div className="flex flex-col text-center my-2 justify-center items-center h-screen">
+            {posts.map((p, index) => (
+              <Card key={index} className="my-2 w-4/6">
+                <CardContent>
+                  <div className=" text-sm">
+                    {p.created_at}
+                    {p.time_created}
+                  </div>
+                  <div>{p.body}</div>
+                </CardContent>
+              </Card>
+            ))}
+            <TextField id="" multiline rows={8} className=" w-4/6"></TextField>
+            <div>
+              <Button variant="text">Post</Button>
+            </div>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }

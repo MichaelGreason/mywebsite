fe4a982789337e799d298baeef1327bbd665dae8
Michael Greason <mgreason18@gmail.com>
2023-06-29 14:29:32 -0400

    home page coming together

--- my-website/src/pages/home.jsx
+++ my-website/src/pages/home.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import Paper from "@mui/material/Paper";
-
+import { Card } from "@mui/material";
 export default function Home() {
   return (
     <>
       <div>
-        <div className="flex w-3/5 font-serif">
-          <Paper elevation={3}>
-            Hello, welcome to the beautoful website made by and for Michael
+        <div className="flex w-3/5 font-serif ml-10 mt-10 text-3xl">
+          <div>
+            Hello, welcome to the beautiful website made by, and for, Michael
             Greason. Michael Greason is a Software Developer, Audio Engineer,
-            Musician, Server, and lover of all things.
-          </Paper>
+            Musician, Server, and lover of all things. Feel free to navigate the
+            site using the "Go To" menu at the top right, enjoy!
+          </div>
         </div>
       </div>
     </>

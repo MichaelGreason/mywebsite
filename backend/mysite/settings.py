667479d4044079e196bf64c8b9dadf8f47daf90c
Michael Greason <mgreason18@gmail.com>
2023-07-18 12:20:00 -0400

    maybe fixed?

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -10,12 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
 import environ
+from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-environ.Env.read_env()
 
 env = environ.Env(
     # set casting, default value
@@ -25,18 +22,25 @@ env = environ.Env(
 )
 
 
+environ.Env.read_env()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
 SECRET_KEY = env('SECRET_KEY')
 
+DEBUG = True
+
+ALLOWED_HOSTS = []
 
 # Application definition
 
